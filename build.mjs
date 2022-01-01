7a425a0309d5df3cc3fbd6881160d2436eb7385a
Max Milton <max@happycode.ltd>
2022-01-01 13:10:25 +1000

    refactor: Update build for better minification

--- build.mjs
+++ build.mjs
@@ -1,19 +1,58 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import esbuild from 'esbuild';
+import { decodeUTF8, encodeUTF8, writeFiles } from 'esbuild-minify-templates';
+import path from 'path';
 
-esbuild
-  .build({
-    entryPoints: ['src/index.ts'],
-    outfile: 'dist/index.js',
-    platform: 'node',
-    target: ['node12'],
-    banner: { js: '"use strict";' },
-    bundle: true,
-    minify: true,
-    legalComments: 'external',
-    logLevel: 'debug',
-  })
-  .catch((error) => {
-    throw error;
-  });
+/** @type {esbuild.Plugin} */
+const analyzeMeta = {
+  name: 'analyze-meta',
+  setup(build) {
+    if (!build.initialOptions.metafile) return;
+
+    build.onEnd(
+      (result) => result.metafile
+        && build.esbuild.analyzeMetafile(result.metafile).then(console.log),
+    );
+  },
+};
+
+/** @type {esbuild.Plugin} */
+const minifyJS = {
+  name: 'minify-js',
+  setup(build) {
+    build.onEnd(async (result) => {
+      if (!result.outputFiles) return;
+
+      for (let index = 0; index < result.outputFiles.length; index++) {
+        const file = result.outputFiles[index];
+
+        if (path.extname(file.path) === '.js') {
+          // eslint-disable-next-line no-await-in-loop
+          const out = await build.esbuild.transform(decodeUTF8(file.contents), {
+            loader: 'js',
+            minify: true,
+          });
+
+          // eslint-disable-next-line no-param-reassign
+          result.outputFiles[index].contents = encodeUTF8(out.code);
+        }
+      }
+    });
+  },
+};
+
+await esbuild.build({
+  entryPoints: ['src/index.ts'],
+  outfile: 'dist/index.js',
+  platform: 'node',
+  target: ['node12'],
+  plugins: [minifyJS, writeFiles(), analyzeMeta],
+  banner: { js: '"use strict";' },
+  bundle: true,
+  minify: true,
+  write: false,
+  legalComments: 'external',
+  metafile: process.stdout.isTTY,
+  logLevel: 'debug',
+});
