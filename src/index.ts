42192fb6afc26cb4df32dc822b382d3178b7034d
Max Milton <max@happyhackers.xyz>
2023-06-05 13:04:20 +1000

    bug: Revert back to an esbuild based build process

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { createRequire } from 'node:module';
 import { run } from './action';
-
-// HACK: Workaround for bun not outputting compatible code for Node.js
-import.meta.require = createRequire(import.meta.url);
 
 void run();
