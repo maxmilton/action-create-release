4c7005defffa201a6f1ede533a46f0dcedfe6dcf
Max Milton <max@happyhackers.xyz>
2023-06-05 12:44:14 +1000

    bug: Add workaround for missing API

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { run } from './action';
 
+// HACK: Workaround for bun not outputting compatible code for Node.js
+// eslint-disable-next-line unicorn/prefer-module
+import.meta.require ??= require;
+
 void run();
